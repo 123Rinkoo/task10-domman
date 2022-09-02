f7800dcd97a724ec20ee0c538e616d75db990a68
Rinkoo <rinkookandpal1966@gmail.com>
2022-09-02 11:22:57 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-console.log("hello this is just for update the javascript file");
-console.log("this is")
+let nme=document.getElementById("name");
+let email=document.getElementById("email")
+let form=document.getElementById("my-form");
+form.addEventListener("submit", nmelocal);
+function nmelocal(e)
+{
+    e.preventDefault();
+    localStorage.setItem("Name",`${nme.value}`);
+    localStorage.setItem("Email",`${email.value}`);
+    
+}
